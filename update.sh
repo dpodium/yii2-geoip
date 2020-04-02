cf7e31d4eca703c18a086aedbf667f00763a7b3f
darren.ng <darren.ng@dpodium.com>
2020-04-02 13:16:27 +0800

    Script update

--- update.sh
+++ update.sh
@@ -1,8 +1,6 @@
 newsha=$(curl "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-Country&license_key=Urbg1xsxwde8bdlQ&suffix=tar.gz.sha256")
 echo $i
 oldsha=$(cat "components/db/sha256")
-echo "$newsha"
-echo "$oldsha"
 
 if [ "$newsha" == "$oldsha" ]; then
     echo "No update."
@@ -15,4 +13,16 @@ tar --strip-components=1 -zxvf "components/db/db.tar.gz" -C "components/db/"
 rm -f "components/db/db.tar.gz"
 git commit -am "db update"
 echo "$newsha" > "components/db/sha256"
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo "----------"
 echo "New update, please tag and push"
+echo "----------"
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
